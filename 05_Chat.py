7ed89e61ac4cf3c2ad3605c4743dec913e4766a9
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-06-21 18:43:40 +0530

    Few-shot Prompting Chain-of-Thought (CoT) Prompting

--- 05_Chat.py
+++ 05_Chat.py
@@ -4,3 +4,26 @@ from openai import OpenAI
 load_dotenv()
 client = OpenAI()   
 
+system_prompt = """
+you are a language model that translates English to French.
+
+Example 1:
+Input: Hello
+Output: Bonjour
+
+Example 2:
+Input: Thank you
+Output: Merci
+
+Now translate the following English text to French:
+"""
+
+result = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        { "role": "system", "content": system_prompt},
+        { "role": "user", "content": "Good Night" } # few short prompting
+    ]
+)
+
+print(result.choices[0].message.content)
---
+++ 06_Chat.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+## Chain-of-Thought Prompting
+
+system_prompt = """"
+you are a only a math tutor. solve problems step by step before giving the final answer.
+
+Example1:
+Input: what is 24 divided by 6?
+Output: First, divide 24 by 6. That gives 4. So, the answer is 4.
+
+
+Example2:
+
+Input: why sky is blue?
+Output: I am math tutor. So, i cannot answer this.
+
+
+
+"""
+
+response = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        # {"role": "user", "content": "what is 2 + 2*0 + 2/2 - 4"} # So the final answer is -1.
+        {"role": "user", "content":"what is the meaning of tokenization"} # I am math tutor. So, i cannot answer this.
+    ]
+    
+)
+
+print(response.choices[0].message.content)
